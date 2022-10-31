9ebe58af7c0bf72a4a30836b7594225c5631fea5
Nikita <prey122@gmail.com>
2022-10-31 20:23:46 +0300

    test

---
+++ index.js
@@ -0,0 +1,6 @@
+var divide = function (dividend, divisor) {
+    return dividend / divisor;
+};
+var hello = true;
+hello = false;
+var bigInt = 100n;
---
+++ index.ts
@@ -0,0 +1,7 @@
+const divide = (dividend: number, divisor: number): number => {
+    return dividend / divisor;
+  }; 
+let hello :boolean = true;
+
+hello = false;
+let bigInt: bigint = 100n;
